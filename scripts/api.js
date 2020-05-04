989a8a14ee3e6b538116a0adc78e31277d100e42
Sergi Vidal Mestre <s.vidal@students.salle.url.edu>
2020-05-04 04:04:03 +0200

    Modified html and css

--- scripts/api.js
+++ scripts/api.js
@@ -1,15 +1,11 @@
 /** ASYNC GET - Create new Player**/
 function createNewPlayer(playerName, callback) {
-    function reqListener () {
-        //var People = eval(this.responseText); // NO
-        // var Player = JSON.parse(this.responseText);
-        // console.log(Player);
-        // console.log(this.responseText);
+    function reqListener() {
         callback(this.responseText);
-}
+    }
 
     var ajaxASYNC_GET = {
-        request: function (url){
+        request: function (url) {
             var xhr = new XMLHttpRequest();
             xhr.addEventListener("load", reqListener);
             xhr.open("GET", url, true);
@@ -17,6 +13,65 @@ function createNewPlayer(playerName, callback) {
         }
     };
 
-    // console.log("http://puigpedros.salleurl.edu/pwi/arena/api/spawn/"+playerName);
-    ajaxASYNC_GET.request("http://puigpedros.salleurl.edu/pwi/arena/api/spawn/"+playerName);
+    ajaxASYNC_GET.request("http://puigpedros.salleurl.edu/pwi/arena/api/spawn/" + playerName);
+}
+
+/** ASYNC GET - Delete current Player**/
+function deleteCurrentPlayer(token) {
+    function reqListener() {
+        alert("El jugador ha sido eliminado correctamente!")
+    }
+
+    var ajaxASYNC_GET = {
+        request: function (url) {
+            var xhr = new XMLHttpRequest();
+            xhr.addEventListener("load", reqListener);
+            xhr.open("GET", url, true);
+            xhr.send();
+        }
+    };
+
+    ajaxASYNC_GET.request("http://puigpedros.salleurl.edu/pwi/arena/api/remove/" + token);
+}
+
+function getCurrentPlayerInfo(token, callback) {
+    function reqListener() {
+        callback(JSON.parse(this.responseText));
+    }
+
+    var ajaxASYNC_GET = {
+        request: function (url) {
+            var xhr = new XMLHttpRequest();
+            xhr.addEventListener("load", reqListener);
+            xhr.open("GET", url, true);
+            xhr.send();
+            // if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            //     callback(JSON.parse(this.responseText));
+
+                // return xhr.responseText;
+            // }else {
+            //     alert("Ha ocurrido un error, posiblemente el usuario ya exista");
+            // }
+        }
+    };
+
+    ajaxASYNC_GET.request("http://puigpedros.salleurl.edu/pwi/arena/api/player/" + token);
+}
+
+/** ASYNC GET - Get Player info**/
+function getNearPlayers(token, callback) {
+    function reqListener() {
+        callback(JSON.parse(this.responseText));
+    }
+
+    var ajaxASYNC_GET = {
+        request: function (url) {
+            var xhr = new XMLHttpRequest();
+            xhr.addEventListener("load", reqListener);
+            xhr.open("GET", url, true);
+            xhr.send();
+        }
+    };
+
+    ajaxASYNC_GET.request("http://puigpedros.salleurl.edu/pwi/arena/api/players/" + token);
 }
--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,6 @@
+let player;
+
+
 /** OnLoad, esta función es la primera que se llama cuando la aplicacion es iniciada **/
 window.onload = function () {
     // var token = createNewPlayer("test0007");
@@ -13,13 +16,17 @@ function initGame() {
 
 /** Initialize objects and variables **/
 function initUI() {
+    addTextToConsole("Bienvenido a Battle Arena! (Desarrollado por <b>Sergi Vidal</b>)");
+
+    createMap();
+    blockReviveDeletePlayerButtons();
     initCreatePlayerForm();
 }
 
 /** Initialize objects and variables **/
 function initObjects() {
-
 }
+
 /**
  * Se encarga de hacer aparecer el formulario de creación del jugador
  */
@@ -37,7 +44,7 @@ function closeCreatePlayerForm() {
 /**
  * Inicializa el formulario para crear el nuevo jugador
  */
-function initCreatePlayerForm(){
+function initCreatePlayerForm() {
     closeCreatePlayerForm();
     document.getElementById('new-player').addEventListener("click", openCreatePlayerFrom);
     document.getElementById('btn-close-create-player').addEventListener("click", closeCreatePlayerForm);
@@ -45,14 +52,89 @@ function initCreatePlayerForm(){
 }
 
 /**
- * Se encarga de obtener el nombre del usuario y llamar a la función encargada de realizar la llamada a la API
+ * A partir del nombre de usuario, se encarga de llamar a las fuciones encargadas de crear el jugador y de obtener su información
  */
 function onClickCreateNewPlayer() {
-    var input_player_name = document.getElementById('input-player-name').value;
-    createNewPlayer(input_player_name, function (result) {
-        var playername = result;
-        console.log(playername);
-
+    var playerName = document.getElementById('input-player-name').value;
+    closeCreatePlayerForm();
+    createNewPlayer(playerName, function (token) {
+        console.log(token);
+        getCurrentPlayerInfo(token, function (object) {
+            player = new Player(object);
+            addTextToConsole("Has creado un nuevo jugador! En la parte superior derecha verás sus estadisticas!");
+            updateViewWithPlayerInfo();
+            enableReviveDeletePlayerButtons();
+        })
     });
 
+}
+
+/**
+ * Función encargada de actualizar informació del usuario en el HTMl
+ */
+function updateViewWithPlayerInfo() {
+    let playerStats = document.getElementById('stats');
+    playerStats.innerHTML = "";
+    createH3Element("Name: " + player.name, playerStats);
+    createH3Element("Attack: " + player.attack, playerStats);
+    createH3Element("Defense: " + player.defense, playerStats);
+    createH3Element("Vitality Points: " + player.vp, playerStats);
+
+}
+
+/**
+ * Función encargada de crear un elemento H3
+ */
+function createH3Element(data, tag) {
+    let node = document.createElement("H3");
+    let textNode = document.createTextNode(data);
+    node.appendChild(textNode);
+    tag.appendChild(node);
+}
+
+/**
+ * Función encargada de bloquear los botones de revivir y eliminar usuario cuando aun no estan creados
+ */
+function blockReviveDeletePlayerButtons() {
+    let reviveBtn = document.getElementById('revive-player');
+    reviveBtn.style.pointerEvents = "none";
+    reviveBtn.style.backgroundColor = "grey";
+
+    let deleteBtn = document.getElementById('delete-player');
+    deleteBtn.style.pointerEvents = "none";
+    deleteBtn.style.backgroundColor = "grey";
+
+}
+
+/**
+ * Función encargada de desbloquear los botones de revivir y eliminar usuario
+ */
+function enableReviveDeletePlayerButtons() {
+    let reviveBtn = document.getElementById('revive-player');
+    reviveBtn.style.pointerEvents = "auto";
+    reviveBtn.style.backgroundColor = "#ec2d42";
+
+    let deleteBtn = document.getElementById('delete-player');
+    deleteBtn.style.pointerEvents = "auto";
+    deleteBtn.style.backgroundColor = "#ec2d42";
+}
+
+//TODO: preguntar como hacer la consola, su tipo de tag y que contenga un scroll!
+function addTextToConsole(text) {
+    let console = document.getElementById('console');
+    console.innerHTML = console.innerHTML + text + "</br>";
+}
+
+function createMap() {
+    let map = document.getElementById('map');
+
+
+    for (let i = 0; i < ROWS; i ++){
+        for(let j = 0; j < COLUMNS; j++){
+            let node = document.createElement("DIV");
+            node.setAttribute("class", "cell");
+            map.appendChild(node);
+
+        }
+    }
 }
